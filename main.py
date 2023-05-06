6da2db1334105ffe0cd2c9cb21ed648cd384cf75
Roger Teong <rogerteong@Rogers-MacBook-Air.local>
2023-05-06 21:49:23 +0800

    added a workable GUI with tkinter

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
 
-image_1 = Image.open(r'/Users/rogerteong/Desktop/Education Transcript/Diploma in CE Year 1.png')
-image_2 = Image.open(r'/Users/rogerteong/Desktop/Education Transcript/Diploma in CE Year 2.png')
-image_3 = Image.open(r'/Users/rogerteong/Desktop/Education Transcript/Diploma in CE Year 3.png')
-image_4 = Image.open(r'/Users/rogerteong/Desktop/Education Transcript/Certificate in Business Transcript.png')
+class Img2PDF_Converter:
+    def __init__(self, master):
+        self.master = master
+        master.title("My GUI")
 
-im_1 = image_1.convert('RGB')
-im_2 = image_2.convert('RGB')
-im_3 = image_3.convert('RGB')
-im_4 = image_4.convert('RGB')
+        self.label = tk.Label(master, text="Select files to open:")
+        self.label.pack()
 
-image_list = [im_2, im_3, im_4]
+        self.button = tk.Button(master, text="Browse files", command=self.on_button_clicked)
+        self.button.pack()
 
-im_1.save(r'/Users/rogerteong/Desktop/Education Transcript/Image to PDF Transcript.pdf', save_all=True, append_images=image_list)
+        self.filename_label = tk.Label(master, text="", wraplength=700, height=10)
+        self.filename_label.pack()
+
+
+    def on_button_clicked(self):
+        filetypes = (("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("All files", "*.*"))
+        filenames = filedialog.askopenfilenames(title="Select files", filetypes=filetypes)
+
+        image_list = []
+
+        if filenames:
+            self.image_filenames = list(filenames)
+            self.filename_label.config(text="Selected files\n" + "\n".join(self.image_filenames))
+            # print("Selected files:")
+            for i, filename in enumerate(self.image_filenames):
+                # print(f"Image{i+1}: {filename}")
+                image_list.append(Image.open(filename).convert('RGB'))
+
+        output_filename = '/Users/rogerteong/Desktop/Education Transcript/output.pdf'
+        with open(output_filename, 'wb') as f:
+            image_list[0].save(f, format='PDF', save_all=True, append_images=image_list[1:])
+
+        
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    my_gui = Img2PDF_Converter(root)
+    root.mainloop()
